0fbb8455b18a817e27d4ad22f30900b813f260eb
wagppinto <wagpinto@me.com>
2015-01-07 15:11:26 -0700

    Done
    All is done

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -19,8 +19,19 @@
 
     // Insert code here...
     
+    int karl = 10;
+    int leo = 7;
+    int karlPlace = (karl > 7) ? 1 : 2;
+    int leoPlace = (leo > 7) ? 1 : 2;
+    
+    if ((karlPlace = 1) && (leoPlace = 1)) { NSLog(@"Both teachers are placed 1");}
+    if ((karlPlace = 1)) { NSLog(@"Karl is placed 1 and Leonard is placed 2");}
+    
+    
     
     return YES;
+    
+    
 }
 
 @end
